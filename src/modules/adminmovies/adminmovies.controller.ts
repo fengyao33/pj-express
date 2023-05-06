8085037959c1131f5620610b134965fa1bfb325a
Leonard <clps935419@gmail.com>
2023-05-07 02:49:21 +0800

    feat:創建後臺電影列表API

---
+++ src/modules/adminmovies/adminmovies.controller.ts
@@ -0,0 +1,55 @@
+import { NextFunction, Request, Response } from 'express'
+import { AdminmoviesService } from './services'
+
+/**
+ * Return all entities
+ * @param req
+ * @param res
+ * @param next
+ */
+export async function index(req: Request, res: Response, next: NextFunction): Promise<void> {
+  const finder = new AdminmoviesService()
+}
+
+/**
+ * Return one instance of entity
+ * @param req
+ * @param res
+ * @param next
+ */
+export async function show(req: Request, res: Response, next: NextFunction): Promise<void> {
+  const { id } = req.params
+  const finder = new AdminmoviesService()
+}
+
+/**
+ * Save an entity
+ * @param req
+ * @param res
+ * @param next
+ */
+export async function store(req: Request, res: Response, next: NextFunction): Promise<void> {
+  const saver = new AdminmoviesService()
+}
+
+/**
+ * Update an entity
+ * @param req
+ * @param res
+ * @param next
+ */
+export async function update(req: Request, res: Response, next: NextFunction): Promise<void> {
+  const { id } = req.params
+  const updater = new AdminmoviesService()
+}
+
+/**
+ * Destroy one instance of an entity
+ * @param req
+ * @param res
+ * @param next
+ */
+export async function destroy(req: Request, res: Response, next: NextFunction): Promise<void> {
+  const { id } = req.params
+  const destroyer = new AdminmoviesService()
+}
