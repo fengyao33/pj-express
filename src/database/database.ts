ecc51c545702c85231c5b2cc6a745d0b4174dc2e
kai <shanecg8v@gmail.com>
2023-05-16 22:36:02 +0800

    test: DB CHANGE

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,10 +1,10 @@
-import { settings } from '@config/settings'
-import mongoose from 'mongoose'
+import { settings } from '@config/settings';
+import mongoose from 'mongoose';
 
+// SK TEST++
 mongoose
   .connect(settings.DB.URI!, {
-    serverSelectionTimeoutMS: 5000
+    serverSelectionTimeoutMS: 5000,
   })
   .then((db) => console.log('Mongo is online...'))
-  .catch((error) => console.error(error))
-
+  .catch((error) => console.error(error));
