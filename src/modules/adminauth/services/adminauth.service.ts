aef58ec06e33d4787f0c192f8fe3847e6b60ff98
cavalown <cavalown@gmail.com>
2023-05-17 00:33:24 +0800

    feat: add admin auth login service

--- src/modules/adminauth/services/adminauth.service.ts
+++ src/modules/adminauth/services/adminauth.service.ts
@@ -1,25 +1,12 @@
+import User from '@models/user.model';
+import bcrypt from "bcryptjs";
+
 export class AdminauthService {
-  async findOne(id: any): Promise<Object> {
-    return {};
-  }
-
-  async findAll(): Promise<Object[]> {
-    return [];
-  }
-
-  async update(id: any, body: any): Promise<Object> {
-    return {};
-  }
-
-  async store(body: any): Promise<Object> {
-    return {};
-  }
-
-  async destroy(id: any): Promise<Object> {
-    return {};
-  }
-
-  async delete(id: any): Promise<Object> {
-    return {};
+  async login(email: string, password: string) {
+    const result = await User.findOne({ email }).select("+password");
+    if (!result) throw new Error("此帳號不存在");
+    const comparePassword = await bcrypt.compare(password, result?.password);
+    if (!comparePassword) throw new Error("密碼錯誤");
+    return result;
   }
 }
