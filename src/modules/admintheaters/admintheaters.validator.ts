62adf3ec3dd441d8870a55e66b32c0a65fbcec08
kai <shanecg8v@gmail.com>
2023-05-24 00:30:44 +0800

    feat: add theater map url and change phone type

--- src/modules/admintheaters/admintheaters.validator.ts
+++ src/modules/admintheaters/admintheaters.validator.ts
@@ -1,5 +1,13 @@
 import { check } from 'express-validator';
 
-export const storeValidators = [];
+export const storeValidators = [
+  check('phone')
+    .matches(/^\(\d{2}\)\d{7}$/)
+    .withMessage('phone 格式錯誤 ex:(02)1234567'),
+];
 
-export const updateValidators = [];
+export const updateValidators = [
+  check('phone')
+    .matches(/^\(\d{2}\)\d{7}$/)
+    .withMessage('phone 格式錯誤 ex:(02)1234567'),
+];
