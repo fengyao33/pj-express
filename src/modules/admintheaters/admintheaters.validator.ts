90f6808310279f0f2567744c3c3cce965d60a508
feng <103472156+fengyao33@users.noreply.github.com>
2023-06-03 02:18:41 +0800

    fix: phone validate has issue

--- src/modules/admintheaters/admintheaters.validator.ts
+++ src/modules/admintheaters/admintheaters.validator.ts
@@ -4,14 +4,14 @@ import multer from 'multer'
 export const storeValidators = [
   check('phone')
     // .matches(/^0\d{1}\-\d{7}$/)
-    .matches(/^0[1-9]\d{7,10}$/)
+    // .matches(/^0[1-9]\d{7,10}$/)
     .withMessage('phone 格式錯誤 ex:02-1234567'),
 ];
 
 export const updateValidators = [
   check('phone')
     // .matches(/^0\d{1}\-\d{7}$/)
-    .matches(/^0[1-9]\d{7,10}$/)
+    // .matches(/^0[1-9]\d{7,10}$/)
     .withMessage('phone 格式錯誤 ex:02-12345678'),
 ];
 
